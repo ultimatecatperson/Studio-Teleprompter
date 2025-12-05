2eedd863d9a4d33146c68be82dc7c38c8ffb5b79
Random Meow <penguinsquad78@gmail.com>
2025-12-05 14:56:35 -0700

    Initial Commit

---
+++ Studio/StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioApp.swift
+//  Studio
+//
+//  Created by Xavier Finch on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
